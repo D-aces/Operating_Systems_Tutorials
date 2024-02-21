ef832ff0c0ff26aad88bd5ab71f661d8b0286fae
noto-21 <128177967+noto-21@users.noreply.github.com>
2024-02-20 21:09:35 -0500

    Commented code

--- Tutorial_5/Q1T5.c
+++ Tutorial_5/Q1T5.c
@@ -1,35 +1,37 @@
-#define _XOPEN_SOURCE 600 // required for barriers to work
+#define _XOPEN_SOURCE 600//Required for barriers to work
 
+/* Libraries */
 #include <stdlib.h>
 #include <stdio.h>
 #include <pthread.h>
 #include <unistd.h>
 
-int timer = 3;
+int timer = 3;//Timer length
 
-void* hello_world()
+void* hello_world()//Prints hello world
 {
 	sleep(rand() % timer + 1);
 	printf("hello world\n");
 	pthread_exit(NULL);
 }
 
-void* goodbye()
+void* goodbye()//Prints goodbye
 {
 	sleep(rand() % timer + 1);
 	printf("goodbye\n");
 	pthread_exit(NULL);
 }
 
+/* Main */
 int main()
 {
-	pthread_t ptid[2];
-	pthread_create (&ptid[0], NULL, hello_world, NULL);
-	pthread_create(&ptid[1], NULL, goodbye, NULL);
+	pthread_t ptid[2];//Thread ID
+	pthread_create (&ptid[0], NULL, hello_world, NULL);//Create thread
+	pthread_create(&ptid[1], NULL, goodbye, NULL);//Create thread
 
 	pthread_join(ptid[0],NULL);
 	pthread_join(ptid[1],NULL);
 
-	return 0;
+	return 0;//Exit
 }
 
