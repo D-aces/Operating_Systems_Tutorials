a2291d3075afb1e5057737149a1185677d9807ca
D-aces <erdmriosnadeau@gmail.com>
2024-03-06 18:43:48 -0500

    Made a correction to q3, now it is complete

--- Tutorial_7/Q3T7.c
+++ Tutorial_7/Q3T7.c
@@ -8,23 +8,37 @@
 #include <sys/wait.h>
 #define PROCESS "./process"
 
-int main(void){	
+void sigint_handler(int sig) {
+	printf("\033[1;31mChild process received SIGINT signal, terminating...\n");
+	printf("\033[0m"); // Reset color
+	exit(EXIT_SUCCESS);
+}
+
+int main(void) {
 	pid_t pid;
+
 	pid = fork();
-	
-	if(pid < 0){
-		return -1;
+
+	if (pid < 0) {
+		perror("fork");
+		return EXIT_FAILURE;
 	}
-	if(pid == 0){
-		
-		execl(PROCESS, PROCESS,  NULL);
-		sleep(5);
-		kill(pid, SIGINT);
-	}
-	else{
-		wait(NULL);	
+
+	if (pid == 0) {
+		// Child process
+		signal(SIGINT, sigint_handler); // Register SIGINT handler
+		execl(PROCESS, PROCESS, NULL); // Executing the specified command
+		perror("execl"); // exec only returns if there is an error
+		return EXIT_FAILURE; // If execl fails
+	} else {
+		// Parent process
+		sleep(5); // Allow child process to start
+		if (kill(pid, SIGINT) != 0) { // Sending SIGINT to the child process
+			perror("kill");
+			return EXIT_FAILURE;
+		}
+		wait(NULL); // Wait for child process to terminate
 	}
 
 	return EXIT_SUCCESS;
 }
-
