10dad7b0933ce46b90ad194e7c8dfa19a759a3ab
D-aces <erdmriosnadeau@gmail.com>
2024-03-06 20:42:57 -0500

    Completed question 4

--- Tutorial_7/Q4T7.c
+++ Tutorial_7/Q4T7.c
@@ -1,4 +1,4 @@
-include <stddef.h>
+#include <stddef.h>
 #include <stdlib.h>
 #include <stdio.h>
 #include <stdbool.h>
@@ -6,7 +6,39 @@ include <stddef.h>
 #include <signal.h>
 #include <sys/types.h>
 #include <sys/wait.h>
-int main(void){
+#define PROCESS "./process"
 
+int main(void) {
+	pid_t pid;
+	int waitPID = 0;
+	int status;
+	pid = fork();
+
+	if (pid < 0) {
+		perror("fork");
+		return EXIT_FAILURE;
+	}
+
+	if (pid == 0) {
+		// Child process
+		execl(PROCESS, PROCESS, NULL); // Executing the specified command
+		perror("execl"); // exec only returns if there is an error
+		return EXIT_FAILURE; // If execl fails
+	} else {
+		// Parent process
+		sleep(5); // Allow child process to start
+		if (kill(pid, SIGTSTP) != 0) { // Sending SIGTSTP to the child process
+			perror("kill");
+			return EXIT_FAILURE;
+		}
+		sleep(10);
+		if (kill(pid, SIGCONT) != 0) { // Sending SIGCONT to the child process
+			perror("kill");
+			return EXIT_FAILURE;
+		}
+		if(waitpid(pid, &status, WUNTRACED) != 0){// Wait for child process to terminate
+			return EXIT_SUCCESS;
+		}
+	}
+	return EXIT_SUCCESS;
 }
-
