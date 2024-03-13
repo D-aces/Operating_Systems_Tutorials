7735c49ee59b3cdf9ffd5eafb7045d726425bff4
D-aces <erdmriosnadeau@gmail.com>
2024-03-13 15:59:24 -0400

    Init Tutorial 7 Folder

---
+++ Tutorial_8/Q2.c
@@ -0,0 +1,13 @@
+#include <stddef.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <stdbool.h>
+#include <unistd.h>
+#include <signal.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+int main(void)
+{
+
+}
+
