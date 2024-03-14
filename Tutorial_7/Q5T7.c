badcdc558f7259876c60d17a2d4c0bb5232bad9e
D-aces <erdmriosnadeau@gmail.com>
2024-03-14 05:53:20 -0400

    Changed ordering of the priority 0 processes, will now be enqued before other procs

--- Tutorial_7/Q5T7.c
+++ Tutorial_7/Q5T7.c
@@ -1,16 +1,15 @@
 #define _XOPEN_SOURCE 700 // required for barriers to work
 
-#include <string.h>
 #include <stddef.h>
 #include <stdlib.h>
 #include <stdio.h>
 #include <stdbool.h>
 #include <unistd.h> 
 #include <signal.h>
+#include <string.h>
 #include <sys/wait.h>
 #include <sys/types.h>
 
-#define MAX_PROCESS 100
 
 typedef struct {
 	char name[256];
@@ -24,19 +23,27 @@ typedef struct Node {
 	struct Node* next;
 } queue;
 
-void push(proc process, queue** head) {
-
-	// Logic to add each process to the linked list (queue).
-	queue* new_node = (queue*)malloc(sizeof(queue));
-	if (new_node == NULL) {
-		printf("Memory allocation failed.");
+void push(proc new_process, queue** head) {
+	queue *current = *head;
+	
+	// If the list is empty, make the new node the head
+	if (current == NULL) {
+		*head = (queue*)malloc(sizeof(queue));
+		(*head)->process = new_process;
+		(*head)->next = NULL;
 		return;
 	}
 
-	new_node->process = process;
-	new_node->next = *head;
-	*head = new_node;
-};
+	// Otherwise, traverse to the end of the list
+	while (current->next != NULL) {
+		current = current->next;
+	}
+
+	// Allocate memory for the new node and insert it at the end
+	current->next = (queue*)malloc(sizeof(queue));
+	current->next->process = new_process;
+	current->next->next = NULL;
+}
 
 proc pop(queue** head) {
 	if (*head == NULL) {
@@ -48,9 +55,7 @@ proc pop(queue** head) {
 		return empty;
 	}
 
-	// Setup a temp next node
 	queue* temp = *head;
-	// 
 	*head = temp->next;
 	proc process = temp->process;
 	free(temp);
@@ -94,37 +99,70 @@ proc* delete_pid(int pid, queue** head) {
 	return NULL;
 }
 
-int main()
-{
+void print_process(proc *process) {
+	printf("Name: %s, Priority: %d, PID: %d, Runtime: %d\n", process->name,
+			process->priority, process->pid, process->runtime);
+}
+
+void print_list(queue *head) {
+	queue* current = head;
+	while(current != NULL) {
+		proc process = current->process;
+		print_process(&process);
+		current = current->next;
+	}
+}
+
+void read_file(queue **head){
 	int init_pid = 0;
-	FILE *file = fopen("processes_q5.txt", "r");
+	proc *hold_lp_process = (proc*)malloc(sizeof(proc));
+	int hold_counter = 0;
+	FILE *file = fopen("processes_q5.txt", "r");	
+	char line[256];	
 
 	if (file == NULL) {
 		printf("error opening file.\n");
-		return 1;
+		exit(0);
 	}
 
-	queue *head = NULL;
-	char line[256];
-	
-
-	// Read file and push priority 0 processes to the struct
+	// Create an instance of the proc struct and add it to the
+	// linked list using the push() function.
 	while (fgets(line, sizeof(line), file)) {
 		proc new_process;
 		new_process.pid = init_pid;
 		sscanf(line, "%[^,],%d,%d", new_process.name, &new_process.priority, &new_process.runtime);
-
-		push(new_process, &head);
-		
+		if(new_process.priority == 0){
+			push(new_process, head);
+		} else {
+			hold_lp_process = (proc *)realloc(hold_lp_process, (hold_counter + 1) * sizeof(proc));			hold_lp_process[hold_counter] = new_process;
+			hold_counter++;
+		}
 	}
 
-	// After all processes have been added to linked list, iterate through and print
-	// name, priority, pid, runtime of each process 
-	queue* current = head;
+	// Add all the held processes to struct
+	if (hold_counter > 0) {
+		for(int i = 0; i < hold_counter; i++){
+			push(hold_lp_process[i], head);
+		}
+	}
+
+	fclose(file);
+	free(hold_lp_process);
+}
+
+int main()
+{	
+	queue *head = NULL;
+
+	// read the process from the file and add them to the linked list	
+	read_file(&head);
+
+	// print the new list, but using pop
+	proc current_process;
 	while (head != NULL) {
-		proc current_process = pop(&head);
-		printf("Name: %s, Priority: %d, PID: %d, Runtime: %d\n", current_process.name,
-				current_process.priority, current_process.pid, current_process.runtime);
+		current_process = pop(&head);
+		print_process(&current_process);
 	}
+	
 	return 0;
 }
